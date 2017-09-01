2c9858d8617d8e1fa4589e3bda56e193a2b233e5
sunshengteng <sunshengteng@yahoo.com>
2017-09-01 17:54:44 +0800

    add:plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
+// webpack.production.config.js
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
     entry: __dirname + "/app/main.js",//已多次提及的唯一入口文件
     output: {
-        path: __dirname + "/public",//打包后的文件存放的地方
-        filename: "bundle.js"//打包后输出文件的文件名
+        path: __dirname + "/build",
+        filename: "bundle-[hash].js"
     },
-    devtool: 'eval-source-map',
+    devtool: 'none',
     devServer: {
-        contentBase: "./public",//本地服务器所加载的页面所在的目录
+        contentBase: "./build",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
-        inline: true//实时刷新
+        inline: true,
+        hot: true
     },
     module: {
         rules: [
-            {  //   导入jsx/js解析模块
+            {
                 test: /(\.jsx|\.js)$/,
                 use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "es2015", "react"
-                        ]
-                    }
+                    loader: "babel-loader"
                 },
                 exclude: /node_modules/
             },
-            {  // 导入css解析模块
+            {
                 test: /\.css$/,
                 use: [
                     {
                         loader: "style-loader"
                     }, {
                         loader: "css-loader",
-                        // 模块化CSS 样式
                         options: {
                             modules: true
                         }
@@ -42,11 +42,13 @@ module.exports = {
             }
         ]
     },
-    plugins: [ // add :plugin
-        // new webpack.BannerPlugin('版权所有，翻版必究'),   // 现在webpack中不带这个插件了，所以需要npm install
+    plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
         new HtmlWebpackPlugin({
-            template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
-        })
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin("style.css")
     ],
 };
-
